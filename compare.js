c0be97f6882a0be1b6c0511925fc679dadc4b6e4
hundekoerper <el.danowar@googlemail.com>
2015-09-28 01:17:14 +0200

    make compare work

--- compare.js
+++ compare.js
@@ -1,18 +1,26 @@
 var comepareProperties = function(usedproperties, database) {
 
-  console.log(usedproperties);
-  console.log(database);
-
+  var matches = []
+  ,   hurensohn = "";
 
     for (var i = 0, x = usedproperties.length; i <= x; i++) {
         if (database.data.hasOwnProperty(usedproperties[i])) {
-            console.log("relevanter scheiss gefunden!");
-            console.log(usedproperties[i]);
-        } else {
-            console.log("kein relevanter scheiss benutzt!")
+            matches.push(usedproperties[i]);
         }
     }
 
+  if (matches.length === 0){
+    console.log("there are no known compatibilty issues in your css-file.")
+  } else {
+      for (var i = 0, x = matches.length; i < x; i++) {
+        console.log("==================");
+        console.log(matches[i]);
+        console.log("------------------");
+        console.log(database.data[matches[i]].title);
+        console.log(database.data[matches[i]].description);
+        console.log("------------------");
+      }
+  }
 };
 
 module.exports = comepareProperties;
