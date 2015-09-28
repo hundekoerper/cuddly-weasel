3adf43397271c1462cc30ee79f954658f9a07114
hundekoerper <el.danowar@googlemail.com>
2015-09-28 02:22:00 +0200

    make ui pretty

--- compare.js
+++ compare.js
@@ -1,7 +1,6 @@
 var comepareProperties = function(usedproperties, database) {
 
-  var matches = []
-  ,   hurensohn = "";
+  var matches = [];
 
     for (var i = 0, x = usedproperties.length; i <= x; i++) {
         if (database.data.hasOwnProperty(usedproperties[i])) {
@@ -10,16 +9,39 @@ var comepareProperties = function(usedproperties, database) {
     }
 
   if (matches.length === 0){
-    console.log("there are no known compatibilty issues in your css-file.")
+    console.log("==================");
+    console.log("there are no known compatibility issues in your css-file.")
+    console.log("==================");
+    console.log("");
   } else {
       for (var i = 0, x = matches.length; i < x; i++) {
         console.log("==================");
         console.log(matches[i]);
-        console.log("------------------");
+        console.log("==================");
         console.log(database.data[matches[i]].title);
+        console.log("");
         console.log(database.data[matches[i]].description);
         console.log("------------------");
+        console.log("Internet Explorer compatibilty:");
+        console.log("");
+          for(var value in database.data[matches[i]].stats.ie){
+            if (database.data[matches[i]].stats.ie[value] === "y") {
+              console.log("Version " + value + ": supported");
+            } else if (database.data[matches[i]].stats.ie[value] === "n") {
+              console.log("Version " + value + ": not supported");
+            } else {
+              console.log("Version " + value + ": partially supported");
+            }
+          }
+        console.log("------------------");
+        console.log("Additional Notes:");
+        console.log("");
+        console.log(database.data[matches[i]].notes);
+        console.log("");
       }
+
+
+
   }
 };
 
